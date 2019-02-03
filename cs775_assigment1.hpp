f68d8ad9598742eb2d43aeb786bed21dbeebd212
Sneha <snehabhakare5@gmail.com>
2019-02-03 23:11:53 +0530

    add report

--- cs775_assigment1.hpp
+++ cs775_assigment1.hpp
@@ -56,8 +56,6 @@ b2World* world=new b2World(b2Vec2(0,0));
 const float height_of_each_elem=3;
 float B=80,L=100,W=1;
 b2Vec2 init_pos(1.5*L/10,-1.5*B/8);
-// b2Vec2 x_range(4*L/10,5*L/10);
-// b2Vec2 y_range(0,B/8);
 b2Vec2 x_range(-2*L/10,-1*L/10);
 b2Vec2 y_range(0,B/8);
 
